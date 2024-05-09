62323684b84dc8159ad0becba8598a4c32b4d246
maria <118778107+mariahlli@users.noreply.github.com>
2024-05-09 22:17:04 +0100

    Update Patient_Discharge_roomRec2.java

--- src/Patient_Discharge_roomRec2.java
+++ src/Patient_Discharge_roomRec2.java
@@ -9,15 +9,7 @@ import java.util.concurrent.TimeUnit;
 import javax.swing.JOptionPane;
 import net.proteanit.sql.DbUtils;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Agarwal
- */
+
 public class Patient_Discharge_roomRec2 extends javax.swing.JFrame {
 Connection con=null;
 ResultSet rs=null;
