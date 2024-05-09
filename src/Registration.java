8f4b1fe6e0b76b6c93a2b54df3cbcecd65d3d720
maria <118778107+mariahlli@users.noreply.github.com>
2024-05-09 22:17:14 +0100

    Update Registration.java

--- src/Registration.java
+++ src/Registration.java
@@ -7,15 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Agarwal
- */
+
 public class Registration extends javax.swing.JFrame {
 Connection con=null;
 ResultSet rs=null;
