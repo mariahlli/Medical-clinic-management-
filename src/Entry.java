fcb1b0389f45237aabd71fed601e5c3c34fcab02
maria <118778107+mariahlli@users.noreply.github.com>
2024-05-09 22:15:24 +0100

    Update Entry.java

--- src/Entry.java
+++ src/Entry.java
@@ -9,15 +9,7 @@ import java.text.SimpleDateFormat;
 import javax.swing.JOptionPane;
 import java.text.SimpleDateFormat;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Agarwal
- */
+
 public class Entry extends javax.swing.JFrame {
 Connection con=null;
 ResultSet rs=null;
