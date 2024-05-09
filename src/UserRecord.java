5afc65f5770796063f7f0195fadb0c35ed6f42a7
maria <118778107+mariahlli@users.noreply.github.com>
2024-05-09 22:18:01 +0100

    Update UserRecord.java

--- src/UserRecord.java
+++ src/UserRecord.java
@@ -1,16 +1,7 @@
 import java.sql.*;
 import javax.swing.*;
-import net.proteanit.sql.DbUtils;/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import net.proteanit.sql.DbUtils;
 
-
-/**
- *
- * @author Agarwal
- */
 public class UserRecord extends javax.swing.JFrame {
 
     Connection con = null;
