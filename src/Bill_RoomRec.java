6084c73d6c4a6a6d5e3375f883ba0483e0369b0d
maria <118778107+mariahlli@users.noreply.github.com>
2024-05-09 22:14:23 +0100

    Update Bill_RoomRec.java

--- src/Bill_RoomRec.java
+++ src/Bill_RoomRec.java
@@ -7,15 +7,7 @@ import java.util.Locale;
 import javax.swing.JOptionPane;
 import net.proteanit.sql.DbUtils;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Agarwal
- */
+
 public class Bill_RoomRec extends javax.swing.JFrame {
     Connection con=null;    
     ResultSet rs=null;
