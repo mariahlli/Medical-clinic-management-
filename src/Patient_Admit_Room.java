71d0931fde07a8b3f5b7e11119bb8ac6d5175d84
maria <118778107+mariahlli@users.noreply.github.com>
2024-05-09 22:16:18 +0100

    Update Patient_Admit_Room.java

--- src/Patient_Admit_Room.java
+++ src/Patient_Admit_Room.java
@@ -8,15 +8,7 @@ import java.sql.Statement;
 import javax.swing.JOptionPane;
 import net.proteanit.sql.DbUtils;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Agarwal
- */
+
 public class Patient_Admit_Room extends javax.swing.JFrame {
     Connection con=null;
     ResultSet rs=null;
